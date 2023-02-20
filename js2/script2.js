573e6bbdd99dd98e09d5f2d39d19e44c47dd09ed
uladziku <kurchich.v@gmail.com>
2023-02-20 08:35:43 +0100

    Create script2.js

---
+++ js2/script2.js
@@ -0,0 +1,7 @@
+let chooseBtn = document.getElementById('choose');
+let receiveBtn = document.getElementById('receive');
+
+let heading = document.getElementsByTagName('h2')[0];
+
+let nameInput = document.getElementsByClassName('contactform_name')[0];
+
