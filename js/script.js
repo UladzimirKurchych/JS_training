5cc95d11af41d2b2cf896627fe7becde94f4a128
uladziku <kurchich.v@gmail.com>
2023-02-27 18:50:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -651,3 +651,13 @@ ask(
 /* eslint prefer-const: 0 */
 
 
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+
+delete myDog.tails;
