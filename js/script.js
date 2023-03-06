9df2ecb08fb425c5920decb64532ccb1cb155c4f
uladziku <kurchich.v@gmail.com>
2023-03-06 11:50:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -650,17 +650,3 @@ ask(
 
 /* eslint prefer-const: 0 */
 
-const ourStorage = {
-  "desk": {
-    "drawer": "stapler"
-  },
-  "cabinet": {
-    "top drawer": { 
-      "folder1": "a file",
-      "folder2": "secrets"
-    },
-    "bottom drawer": "soda"
-  }
-};
-
-console.log(ourStorage.cabinet['top drawer'].folder2);
