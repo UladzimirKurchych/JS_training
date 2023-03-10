8361f21027341e7d8cd065a29616cde35596e59e
uladziku <kurchich.v@gmail.com>
2023-03-10 11:07:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -652,34 +652,6 @@ ask(
 
 // Setup
 
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
+let a = '' ;
 
-// Only change code below this line
-function updateRecords(records, id, prop, value) {
-	if (prop !== 'tracks' && value !== undefined) {
-		records[id][prop] = value;
-	}
-  return records;
-}
-
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-
-console.log(recordCollection[5439].artist);
+console.log(typeof a);
